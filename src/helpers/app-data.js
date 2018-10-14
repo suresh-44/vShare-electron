193102d5d97b53b4fe12814b1af5b58f9f008b90
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-14 11:47:26 +0530

    added helper for appData.

---
+++ src/helpers/app-data.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const appData = {
+  appDataFile: "app-data.json",
+
+  write: (key, value) => {
+
+  },
+
+  get: (key) => {
+
+  },
+
+  modify: (key, value) => {
+
+  },
+
+  exists: (key) => {
+
+  }
+
+};
+
+module.exports = appData;
