ccd93346040d087d054bdfb4282d13f1efe2b75a
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-14 15:45:21 +0530

    added jsonfile lib to write, fetch app data.

--- src/helpers/app-data.js
+++ src/helpers/app-data.js
@@ -1,20 +1,32 @@
+import jsonfile from "jsonfile";
+
 const appData = {
   appDataFile: "app-data.json",
 
   write: (key, value) => {
-
+    let json = jsonfile.readFileSync(this.appDataFile);
+    json[key] = value;
+    return jsonfile.writeFile(this.appDataFile, json);
   },
 
   get: (key) => {
-
+    if (this.exists(key)) {
+      let json = jsonfile.readFileSync(this.appDataFile);
+      return json[key];
+    } else {
+      throw key + " does not exists"
+    }
   },
 
   modify: (key, value) => {
-
+    let json = jsonfile.readFileSync(this.appDataFile);
+    json[key] = value;
+    return jsonfile.writeFile(this.appDataFile, json);
   },
 
   exists: (key) => {
-
+    let json = jsonfile.readFileSync(this.appDataFile);
+    return (key in json);
   }
 
 };
