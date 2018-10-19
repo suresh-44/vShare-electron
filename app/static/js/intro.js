3a83f0ce343f54f5004cc4d241783d82256c5343
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-19 15:24:07 +0530

    design of intro.html

--- app/static/js/intro.js
+++ app/static/js/intro.js
@@ -8,8 +8,26 @@ let advanceSlider = function (advanceTo) {
 
 let previousSlide = function (backTo) {
   let id = "disp" + backTo;
-  document.getElementById(id).style.display = "none";
+  document.getElementById(id).style.display = "block";
 
   id = "disp" + (backTo + 1);
-  document.getElementById(id).style.display = "block";
+  document.getElementById(id).style.display = "none";
+};
+
+document.getElementById('next-btn-1').onclick = () => {
+  advanceSlider(2)
+};
+document.getElementById('next-btn-2').onclick = () => {
+  advanceSlider(3)
+};
+
+document.getElementById('prev-btn-1').onclick = () => {
+  previousSlide(1)
+};
+document.getElementById('prev-btn-2').onclick = () => {
+  previousSlide(2)
+};
+
+document.getElementById('continue-btn').onclick = () => {
+
 };
