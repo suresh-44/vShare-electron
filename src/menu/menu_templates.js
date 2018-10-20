b5f6675d01bbb81f7302de922366af3795830c2b
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-20 18:45:15 +0530

    added dummy click functions to menu items.

--- src/menu/menu_templates.js
+++ src/menu/menu_templates.js
@@ -4,18 +4,30 @@ export const fileMenuTemplate = {
     {
       label: "Send",
       accelerator: "Alt+Ctrl+S",
+      click: () => {
+
+      }
     },
     {
       label: "Receive",
       accelerator: "Alt+Ctrl+R",
+      click: () => {
+
+      }
     },
     {
       label: "Settings",
       accelerator: "Alt+Shift+S",
+      click: () => {
+
+      }
     },
     {
       label: "Exit",
       accelerator: "Alt+Ctrl+Q",
+      click: () => {
+
+      }
     }
   ]
 };
@@ -25,10 +37,16 @@ export const aboutMenuTemplate = {
   submenu: [
     {
       label: "About",
+      click: () => {
+
+      }
     },
     {
       label: "Help",
       accelerator: "F1",
+      click: () => {
+
+      }
     }
   ]
 };
