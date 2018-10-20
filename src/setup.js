29d29bb9b08ca7f4bb0e6a6d514eab5e16091dde
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-20 11:57:49 +0530

    added secure to setup.js

--- src/setup.js
+++ src/setup.js
@@ -1,5 +1,6 @@
 import firstRun from "./helpers/first-run";
 import appData from "./helpers/app-data";
+import "./secure";
 
 document.getElementById("finish").onclick = () => {
   let username = document.getElementById("username").value;
