9a537637c97f3eabfdbf0b83967468980e3bd2b0
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-20 11:52:40 +0530

    functions organization.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,10 @@
 //import "./stylesheets/main.css";
 import "./helpers/context_menu.js";
 import "./helpers/external_links.js";
-import {remote} from "electron";
-import firstRun from "./helpers/first-run";
 import "./secure";
-import url from "url";
-import path from "path";
+import {firstCheck} from "./helpers/appFunctions";
 
-const app = remote.app;
-const currentWindow = remote.getCurrentWindow();
-
-console.log("starting app.js");
-
-const firstCheck = () => {
-  console.log("Checking if app is running for first time");
-  if (firstRun.isFirstRun()) {
-    console.log("......App is running for first time");
-
-    currentWindow.loadURL(
-      url.format({
-        pathname: path.join(__dirname, "intro.html"),
-        protocol: "file:",
-        slashes: true
-      })
-    );
-  }
-};
 
 window.onload = firstCheck;
 
-//document.addEventListener("load", firstCheck);
 
---
+++ src/helpers/appFunctions.js
@@ -0,0 +1,26 @@
+import firstRun from "./first-run";
+import url from "url";
+import path from "path";
+import receiveServer from "../receiver-server";
+import {remote} from "electron";
+
+const currentWindow = remote.getCurrentWindow();
+
+export const firstCheck = () => {
+  console.log("Checking if app is running for first time");
+  if (firstRun.isFirstRun()) {
+    console.log("......App is running for first time");
+
+    currentWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "intro.html"),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+  }
+};
+
+export const enterReceiveMode = () => {
+  receiveServer.start();
+};
